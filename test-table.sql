188bcd33f2f1ec590a1dca5912029f6fffb1fde0
Marcus Fields <fieldsmu@gmail.com>
2018-05-24 10:31:00 -0400

    added test-table

---
+++ test-table.sql
@@ -0,0 +1,27 @@
+/*
+drop table test;
+
+create table test (
+	id int not null identity(1, 1) primary key,
+	name nvarchar(30) not null
+);
+
+alter table test 
+	add city nvarchar(30) not null;
+alter table test 
+	alter column city nvarchar(30) not null;
+alter table test
+	add zip nvarchar(5) not null;
+alter table test 
+	drop column zip
+
+insert test 
+	(name, zip, city)
+	values 
+	('Marcus', 45069, 'West Chester')
+
+update test set city = 'Mason' where zip = 45069;
+
+select * from test;
+
+*/
